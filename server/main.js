265f8d8445e5ebfcf86a368e248ef34302599d76
Pandey, Brijesh (ES) <brijesh.pandey@adp.com>
2017-10-12 00:44:43 +0530

    Removing port details

--- server/main.js
+++ server/main.js
@@ -9,14 +9,10 @@ var express = require('express'),
   login = require('./routes/login');
 
 var app = new express();
-var PORT = process.env.HEROKU_NODEJS_PORT || 8098;
-var server_ip_address = process.env.HEROKU_NODEJS_IP || '127.0.0.1'
 
 app.get('/', function (req, res) {
   res.send('hello world');
-}).listen(PORT, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + PORT )
-});
+})
 
 app.use(parser.json());
 app.use('/profile',  profiles);
